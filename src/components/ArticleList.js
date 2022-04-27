5714c3c8e64510111d2017373e0657591cbf29fe
Eric Xiao <es.xiao93@gmail.com>
2022-04-27 10:12:09 -0400

    commit tree 122a081b4d096e97b672bf395a993cae1361446c

---
+++ src/components/ArticleList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Article from "./Article";
+
+function ArticleList({posts}) {
+
+    const postList = posts.map ((post) => {
+        return <Article id={post.id} title={post.title} date={post.date} preview={post.preview} />
+
+    })
+
+  return (
+    <main>
+        {postList}
+    </main>
+  )
+}
+
+export default ArticleList;
